76f180013b97ee59c75b871a82df492a0b31e7bf
camekman <ekman.camilla@gmail.com>
2022-01-27 21:15:21 +0100

    bulit the fetch req function for uploading and displaying images with the reducer. could not get it to work ... yet..

--- frontend/src/components/UploadImage.js
+++ frontend/src/components/UploadImage.js
@@ -1,16 +1,25 @@
 import React, { useState, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch, batch } from "react-redux";
 import styled from "styled-components";
+
+import wardrobe from "../reducers/wardrobe";
+import user from "../reducers/user";
 
 const UploadImage = () => {
   const fileInput = useRef();
+
+  const [imageId, setImageId] = useState("");
   const [name, setName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-  const [galleries, setGalleries] = useState("");
+  const [error, setError] = useState("");
+
+  const [galleries, setGalleries] = useState([{}]);
 
   // const galleries = useSelector((store) => store.user.galleries);
   const userId = useSelector((store) => store.user.userId);
   const UPLOAD_URL = `http://localhost:8080/upload/${userId}`;
+
+  const dispatch = useDispatch();
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -26,12 +35,34 @@ const UploadImage = () => {
     fetch(UPLOAD_URL, options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        setGalleries(data.galleries);
-        setImageUrl(data.imageUrl);
-      }, [])
-      .catch((err) => console.log(err));
+        // console.log(data);
+        console.log(data.galleries);
+        console.log(data.galleries[""]);
+        console.log(data.galleries[0]);
+        console.log(data.galleries[{}]);
+        console.log(data.galleries.length);
+
+        if (data.success) {
+          batch(() => {
+            dispatch(wardrobe.actions.setImageId(data.response.imageId));
+            dispatch(wardrobe.actions.setName(data.response.name));
+            dispatch(wardrobe.actions.setImageUrl(data.response.imageUrl));
+            dispatch(user.actions.setGalleries(data.response.galleries));
+            dispatch(wardrobe.actions.setError(null));
+          });
+        } else {
+          batch(() => {
+            dispatch(wardrobe.actions.setImageId(null));
+            dispatch(wardrobe.actions.setName(null));
+            dispatch(wardrobe.actions.setImageUrl(null));
+            dispatch(user.actions.setGalleries(null));
+            dispatch(wardrobe.actions.setError(data.response));
+          });
+        }
+      });
   };
+
+  // do we need a get req also?
 
   return (
     <>
@@ -40,7 +71,11 @@ const UploadImage = () => {
         <StyledForm onSubmit={handleFormSubmit}>
           <UploadSection>
             <p>Choose your image here:</p>
-            <input type="file" ref={fileInput} />
+            <input
+              type="file"
+              ref={fileInput}
+              // onChange={(e) => setGalleries(e.target.ref)}
+            />
           </UploadSection>
 
           <label>
@@ -58,8 +93,22 @@ const UploadImage = () => {
         <div>
           <h1>Uploaded image will be displayed here</h1>
         </div>
+        <img src={imageUrl} />
+        {/* <img src={imageUrl.data} /> */}
+        {/* <img src={imageUrl[""].response} /> */}
+        {/* <img src={imageUrl[0].response} /> */}
+        {/* <img src={setImageUrl} /> */}
+        {/* <img src={fileInput} /> */}
+        {/* <img src={galleries[0]} /> */}
         <img src={imageUrl.galleries} />
-        <img src={galleries} />
+        <img src={galleries[0]} />
+        {/* <img src={galleries[{}]} /> */}
+        {/* <img src={imageUrl.galleries[0]} /> */}
+
+        <p>{name}</p>
+        <p>{imageId}</p>
+        <p>{error.response}</p>
+
         {/* <div>
           {galleries.map((item) => (
             <div key={userId}>
