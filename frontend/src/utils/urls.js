aa6760400bd69dccf37a6e0e009a600d120df574
Charlotte Nyman <85078175+Svempolin@users.noreply.github.com>
2022-02-18 11:25:39 +0100

    trying to connect BE  with FE

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "http://localhost:8080";
 const BASE_URL = "https://susta-wardrobe.herokuapp.com/";
 
-export const API_URL = (slug) => `${BASE_URL}/${slug}`;
+export const API_URL = (slug) => `${BASE_URL}${slug}`;
